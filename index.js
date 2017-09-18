c585c92d2978e6a2d87f7cd6263e26cfa0caceca
Unknown <pe0877888@otc.edu>
2017-09-18 17:41:56 -0500

    Finished i think

--- index.js
+++ index.js
@@ -2,38 +2,35 @@ const _ = require("lodash")
 const M = require("moment")
 const chalk = require('chalk')
 const MomentRange = require('moment-range')
-
-
 const moment = MomentRange.extendMoment(M)
 
 let year = moment().range('year')
 
-
-
-for(const month of year. by('month'))
+for(const month of year.by('month'))
 {
     console.log(_.pad(month.format('MMMM'), 26, '-'))
     console.log('S   M   T   W   Th   F   S ')
-
-    let monthRange = month.range('month')
-    let firstDay = monthRange.start.day() 
-
-    const chunkSize = 7;
-
+    
     let days =  Array.from(month.range('month').by('days'))
+ 
     let paddedDays = _.map(days, day => {
         let date = day.date()
         
         if (day.month() == 8 && day.date() == 10) {
-            date = chalk.red(date)
+            date = chalk.blue(date)
         }
         if (day.month() == 2 && day.date() == 5) {
             date = chalk.green(date)
         }
-
-        return _.padEnd(date, 2)
-
+        return _.padStart(date, 2 )
+        return _.padEnd(date, 2, ' ')  
     })
 
-    console.log(paddedDays, firstDay)
+    for (i = 0; i < month.day(); i++)   
+      {
+      paddedDays.unshift('  ');    
+      }
+
+      paddedDays = _.chunk(paddedDays, 7);
+      paddedDays.forEach(week => {console.log(week.join("  "))})  
 }
