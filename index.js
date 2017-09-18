63dff60f4920fddc62d5dfcfbe4a19714db8efc6
Unknown <pe0877888@otc.edu>
2017-09-18 18:28:21 -0500

    Commented

--- index.js
+++ index.js
@@ -4,33 +4,39 @@ const chalk = require('chalk')
 const MomentRange = require('moment-range')
 const moment = MomentRange.extendMoment(M)
 
-let year = moment().range('year')
+//Will create the year
+let year = moment().range('year') 
 
-for(const month of year.by('month'))
+//For each month of the year Print the days
+for(const month of year.by('month')) 
 {
+  // will print the days of week
     console.log(_.pad(month.format('MMMM'), 26, '-'))
     console.log('S   M   T   W   Th   F   S ')
-    
+    //Creats days array
     let days =  Array.from(month.range('month').by('days'))
  
     let paddedDays = _.map(days, day => {
         let date = day.date()
-        
+        //Will highlight Adams bithday
         if (day.month() == 8 && day.date() == 10) {
             date = chalk.blue(date)
         }
+        //Will highlite my birthday
         if (day.month() == 2 && day.date() == 5) {
             date = chalk.green(date)
         }
         return _.padStart(date, 2 )
-        return _.padEnd(date, 2, ' ')  
+        return _.padEnd(date, 2)  
     })
-
+    //Will print the dates of the month 
     for (i = 0; i < month.day(); i++)   
       {
-      paddedDays.unshift('  ');    
+        //Will add the spaces to the beggining of the days array
+      paddedDays.unshift('  ');   
       }
-
+      //Will chunk togeather the days into chunks of 7
       paddedDays = _.chunk(paddedDays, 7);
+      //Will display days foreach week and join on Week then display
       paddedDays.forEach(week => {console.log(week.join("  "))})  
 }
